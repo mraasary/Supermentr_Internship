18c527d625784e4cfc333bcff64332ad0b3eeee5
Aashish Chaudhary <mraasary@gmail.com>
2026-02-20 13:08:10 +0530

    Add files via upload

---
+++ Assignments/Logic_Builder.py
@@ -0,0 +1,28 @@
+"""Assignment Name : Logic Builder
+Description : Print numbers 1–50 with Fizz/Buzz logic and count occurrences using loops and functions."""
+
+def fizz_buzz():
+    fizz=buzz=fizzbuzz=0
+    
+    for num in range(1,51):
+        if num%3==0 and num%5==0:
+            print("FizzBuzz")
+            fizzbuzz+=1
+        elif num%3==0:
+            print("Fizz")
+            fizz+=1
+        elif num%5==0:
+            print("Buzz")
+            buzz+=1
+        else:
+            print(num)
+            
+    print("\nSummary:")
+    print(f"Total Fizz: {fizz}")
+    print(f"Total Buzz: {buzz}")
+    print(f"Total FizzBuzz: {fizzbuzz}")
+
+if __name__ == "__main__":
+    fizz_buzz()
+    
+            
